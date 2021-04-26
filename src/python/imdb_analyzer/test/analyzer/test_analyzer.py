acdab549f5c813a55f69313f4c227b09d2884dcd
Harris <harris.joseph.088@gmail.com>
2021-04-26 00:45:47 -0400

    adding almost all unit tests

--- src/python/imdb_analyzer/test/analyzer/test_analyzer.py
+++ src/python/imdb_analyzer/test/analyzer/test_analyzer.py
@@ -6,7 +6,7 @@ sys.path.insert(0,os.path.abspath('../../'))
 from analyzer import *
 
 #build small table I can hand calculate on to verify
-TEST_COLUMN_NAMES = set({'actor_1_name','actor_2_name','actor_3_name','genres','budget','gross'})
+TEST_COLUMN_NAMES = ['actor_1_name','actor_2_name','actor_3_name','genres','budget','gross']
 TEST_COLUMNS = [
     ['Aaahnold','Lucille Ball','Aaahnold','Aaahnold','Vinnie Jones','Lucille Ball'],
     ['Lucille Ball','Aaahnold','Vinnie Jones','Lucille Ball','Aaahnold','Vinnie Jones'],
@@ -22,34 +22,65 @@ TEST_COLUMNS = [
     # Set Mel gibsons row to zero gross
     [1000,10000,0,500,1500,10000]
 ]
-test_df = pd.DataFrame(
+
+TEST_DF = pd.DataFrame(
     dict(zip(TEST_COLUMN_NAMES,TEST_COLUMNS))
+    ,index=range(6)
 )
 
+# just making sure everything works together as external library
 def full_run():
     movies = read_file(DATA_PATH)
     top_10_genres_avg = calculate_top_gross_profit(data_frame=movies,colnames=['genres'],metric='mean')
     top_10_genres_total = calculate_top_gross_profit(data_frame=movies,colnames=['genres'])
-    # A lot of movies had zero budget
-    # I'll have to find a more robust formula for marginal roi
+    # A lot of movies had zero budget which caused infinity marginal roi
     # grouped_aggregated_genres_marginal = calculate_top_marginal_roi(movies=movies,colname='genres')
     # print(top_10_genres)
     # print(grouped_aggregated_genres_marginal)
+
     # no not for collective baragaing xD
     # there are 3 actor columns, so I union them into a single col
     actors_union = union_actor_columns(movies)
     top_10_actors_avg = calculate_top_gross_profit(data_frame=actors_union,colnames=['actor_name'],metric='mean')
     top_10_actors_total = calculate_top_gross_profit(data_frame=actors_union,colnames=['actor_name'])
-
     print(top_10_actors_avg)
     print(top_10_actors_total)
 
 def main():
-    print(test_df)
-
-    full_run()
-    
-
+    movies = TEST_DF    
+    top_genres_mean = calculate_top_gross_profit(data_frame=movies,colnames=['genres'],top_n=6,metric='mean')
+    print('Top genres (metric average):')
+    print(top_genres_mean)
+    # I calculated these by hand to verify
+    print('Verifying top grossing genre is scifi...')
+    assert top_genres_mean.index[0] == 'scifi'
+    print('Verifying Scifi Averages...')
+    assert top_genres_mean.loc['scifi']['gross_profit'] == 10000
+    assert top_genres_mean.loc['scifi']['gross'] == 10000
+    assert top_genres_mean.loc['scifi']['budget'] == 0
+    print('Scifi Averages Verified! Verifying Action Averages...')
+    assert top_genres_mean.loc['action']['gross_profit'] == 4925
+    assert top_genres_mean.loc['action']['gross'] == 5000
+    assert top_genres_mean.loc['action']['budget'] == 75
+    print('Scifi Averages Verified! Verifying Comedy Averages...')
+    assert top_genres_mean.loc['comedy']['gross_profit'] == 705
+    assert top_genres_mean.loc['comedy']['gross'] == 750
+    assert top_genres_mean.loc['comedy']['budget'] == 45
+    print('Scifi Averages Verified! Verifying Rom Com Averages...')
+    assert top_genres_mean.loc['rom com']['gross_profit'] == 500
+    assert top_genres_mean.loc['rom com']['gross'] == 1500
+    assert top_genres_mean.loc['rom com']['budget'] == 1000
+    print('All tests passed for Average metric!')
+    print('Top Actors (metric sum):')
+    actors_union = union_actor_columns(TEST_DF)
+    top_actors_gross_sum = calculate_top_gross_profit(data_frame=actors_union,colnames=['actor_name'],top_n=6,metric='sum')
+    print(top_actors_gross_sum)
+    print('Verifying first Lucille Ball and last place Mel Gibson...')
+    assert top_actors_gross_sum.index[0] == 'Lucille Ball'
+    assert top_actors_gross_sum.index[3] == 'Mel Gibson'
+    print('First and last Verified, verifying Lucille Ball stats...')
+    # TODO finish unit tests for stats
+    # full_run()
 
 if __name__=='__main__':
     main()
